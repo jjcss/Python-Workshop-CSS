3bb8cc5f4bd1f65c10b78321f487100089273a36
Computer Science Society Club at John Jay College <33881291+jjcss@users.noreply.github.com>
2021-09-23 21:32:51 -0400

    Add files via upload

---
+++ StarterFiles/Python-Workshop-CSS-main/carManager.py
@@ -0,0 +1,30 @@
+# import turtle from Turtle
+
+# import random
+
+"""Global Variables"""
+
+# List of colors (to pick at random)
+
+# Starting Moving distance of the cars on each refresh
+
+# Moving increment of the cars 
+
+"""Class declaration"""
+
+# class CarManager 
+
+"""Car manager class pupose is to generate all the random cars and move them across the screen """
+    # Defining the class
+
+        # List of all cars
+
+        # Car speed which is the moving distances
+
+"""Methods of the class"""
+
+    # Create car method
+
+    # Move method
+
+    # Increase speed method
---
+++ StarterFiles/Python-Workshop-CSS-main/player.py
@@ -0,0 +1,32 @@
+# import turtle from Turtle
+
+"""Global Variables"""
+# Starting position
+
+# Moving (how much the turtle moves each time)
+
+#Finish Line Y-axis position
+
+"""Class declaration"""
+# class Player with a super class Turtle
+
+    # Defining the class
+"""Player class purpose is to manage the turtle we're controlling to cross the road"""
+        # Initialize with super class
+
+        # Shape of the turle
+
+        # Color of turtle?
+
+        # "Pen" up (prevents the "turtle" from drawing a line after moving)
+
+        # Reset player method
+
+        # Set the heading of the "turtle"
+
+"""Methods of the class"""   
+    # Move up method
+
+    # Method to reset to starting position
+
+    # Method for what happens when player gets to finish line
